ee1aca6e6f7cc9edadb2b44e36763f4c7f0a746d
Ալեքսանդր Արաքելյան <bambrikii@gmail.com>
2022-01-01 14:50:07 -0500

    - router removed, - TODO: PrivateRouter .

--- translator-web/src/js/common/PrivateRoute.tsx
+++ translator-web/src/js/common/PrivateRoute.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  Route
-} from "react-router-dom";
+import { Route } from "../route/Route";
 
 export const PrivateRoute = ({authenticated, currentUser, children, path, exact = false}) => {
   return (<Route path={ path }>

365fd974e3da2d3d6968ca609d8395b748497185
Ալեքսանդր Արաքելյան <bambrikii@gmail.com>
2022-09-14 23:33:12 -0400

    - conditional virtualization.

--- export-impl/src/test/java/org/bambrikii/lang/pagetranslator/export/ExportServiceTest.java
+++ export-impl/src/test/java/org/bambrikii/lang/pagetranslator/export/ExportServiceTest.java
@@ -2,8 +2,10 @@ package org.bambrikii.lang.pagetranslator.export;
 
 import org.bambrikii.lang.pagetranslator.export.config.ExportConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@EnableAutoConfiguration
 @SpringBootTest(classes = ExportConfig.class)
 public class ExportServiceTest {
     @Test

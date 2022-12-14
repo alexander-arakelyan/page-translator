a4c157f1eae8a8bb88f82d5114f1c2b5cc9fa283
Alexander Arakelyan <alexander.arakelyan@gmail.com>
2022-12-13 21:31:59 -0500

    - npm dependencies, - export web client buffer size.

--- translator-web/src/main/java/org/bambrikii/lang/pagetranslator/config/WebClientConfigs.java
+++ translator-web/src/main/java/org/bambrikii/lang/pagetranslator/config/WebClientConfigs.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -19,10 +20,14 @@ public class WebClientConfigs {
 
     @Bean
     public WebClient exportWebClient() {
+        final int size = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
         return WebClient.builder()
                 .baseUrl(pageTranslatorProps.getExportServiceUrl())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE)
-                .baseUrl(pageTranslatorProps.getExportServiceUrl())
+                .exchangeStrategies(strategies)
                 .build();
     }
 }
